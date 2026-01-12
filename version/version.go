895a5f3a057c44096cf71b00a182211bf468b21d
Hanif <hihanifm@gmail.com>
2026-01-12 14:04:24 -0500

    Bump version to 2.17.0

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of Windows Network Manager and Scanner
-const Version = "2.16.0"
+const Version = "2.17.0"
 
 // ServiceName is the service name for Windows Network Manager
 const ServiceName = "WindowsNetworkManager"
